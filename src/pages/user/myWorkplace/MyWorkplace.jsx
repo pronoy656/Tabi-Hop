be17badab6b442c2d15b4bab4ca2c769793bacb1
Ruhul Amin <ruhulthisis@gmail.com>
2025-07-25 05:51:51 +0600

    My workPlace Page start

--- src/pages/user/myWorkplace/MyWorkplace.jsx
+++ src/pages/user/myWorkplace/MyWorkplace.jsx
@@ -1,9 +1,83 @@
-
-
 const MyWorkplace = () => {
+  const reminders = [
+    {
+      id: 1,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 7,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 6,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 5,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 4,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 3,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+    {
+      id: 2,
+      time: "12 pm",
+      month: "april",
+      des: "Lorem Ipsum is simply dummy text of the printing",
+      tag: "event",
+    },
+  ];
   return (
-    <div>MyWorkplace</div>
-  )
-}
+    <section className="p-6 bg-[#e6f7fa] rounded-md border border-[#ACE0E5]">
+      {/* reminders */}
 
-export default MyWorkplace
+      <div
+        className="bg-[#ffffff] border rounded-xl border-gray-300  p-10"
+        style={{ font: "Inter" }}
+      >
+        <h1 className="text-3xl font-bold">Upcoming Reminders</h1>
+
+        <div className="flex flex-1 gap-4 mt-4">
+          {reminders?.map((reminder, index) => (
+            <div
+              key={index}
+              className="bg-[#D8FFE3] border rounded-xl border-gray-300  p-4 space-y-2"
+            >
+              <h1 className="uppercase text-xl font-semibold text-[#000000]">
+                {reminder.month} - {reminder.time}
+              </h1>
+
+              <p className="text-[#394050] font-medium text-base">{reminder.des}</p>
+              <p className="text-[#394050] font-medium capitalize">{reminder.tag}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyWorkplace;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,15 +6,12 @@ import SignUp from "../pages/authentication/SignUp/SignUp";
 import MyWorkplace from "../pages/user/myWorkplace/MyWorkplace";
 import TravelSelector from "../pages/authentication/TravelSelector/TravelSelector";
 import FlowSelector from "../pages/authentication/FlowSelector/FlowSelector";
-<<<<<<< HEAD
 import Accommodations from "../pages/user/accommodations/Accommodations";
-=======
 import ActivitySelector from "../pages/authentication/ActivitySelector/ActivitySelector";
 import DislikeSelector from "../pages/authentication/DislikeSelector/DislikeSelector";
 import StressSelector from "../pages/authentication/StressSelector/StressSelector";
 import EmailVerefication from "../pages/authentication/Emailverefication/EmailVerefication";
 import BirthdaySelection from "../pages/authentication/BIrthdaySelection/BirthdaySelection";
->>>>>>> 44ad727495da62ebf1b9b184a722615ab2963073
 
 const router = createBrowserRouter([
   {
