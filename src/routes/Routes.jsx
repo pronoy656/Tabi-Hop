7e476d21ce875163b280414cdf5cfe2150b38673
Ruhul Amin <ruhulthisis@gmail.com>
2025-07-27 22:39:16 +0600

    gg

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -12,11 +12,9 @@ import DislikeSelector from "../pages/authentication/DislikeSelector/DislikeSele
 import StressSelector from "../pages/authentication/StressSelector/StressSelector";
 import EmailVerefication from "../pages/authentication/Emailverefication/EmailVerefication";
 import BirthdaySelection from "../pages/authentication/BIrthdaySelection/BirthdaySelection";
-
 import WorkplaceIterinaryDetails from "../components/Workplace/WorkplaceIterinaryDetails";
 import Itinerary from "../pages/user/Itinerary/Itinerary";
 import CreateItinerary from "../components/Itinerary/CreateItinerary";
-
 import DateLocationSelect from "../pages/authentication/DateLocationSelect/DateLocationSelect";
 import AddDestination from "../components/Itinerary/AddDestination";
 import MoodBoard from "../pages/user/moodboard/MoodBoard";
