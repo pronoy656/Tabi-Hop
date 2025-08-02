6ff2baeb8265733909abd9e5d924063ba404e4aa
Ruhul Amin <ruhulthisis@gmail.com>
2025-08-03 03:39:11 +0600

    Add Mood Board Modal Complete

--- src/pages/user/moodboard/MoodBoard.jsx
+++ src/pages/user/moodboard/MoodBoard.jsx
@@ -9,7 +9,11 @@ import SectionHeader from "../../../components/shared/SectionHeader"
 const MoodBoard = () => {
  const [isModalOpen, setIsModalOpen] = useState(false);
   const [moodBoards,setMoodBoards] = useState([
+
   { hero: "/moodboard/moodboard1.png", title: "Hello Cyber Bunny" ,media:"/iterenary.jpg,",location:"Head southwest on 5th Ave toward E 41st St",summary:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",socialLink1:"facebook",socialLink2:"facebook",socialLink3:"facebook"},
+  { hero: "/moodboard/moodboard-2.png", title: "Hello Cyber Bunny" ,media:"/iterenary.jpg,",location:"Head southwest on 5th Ave toward E 41st St",summary:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",socialLink1:"facebook",socialLink2:"facebook",socialLink3:"facebook"},
+  { hero: "/moodboard/moodboard-3.png", title: "Hello Cyber Bunny" ,media:"/iterenary.jpg,",location:"Head southwest on 5th Ave toward E 41st St",summary:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",socialLink1:"facebook",socialLink2:"facebook",socialLink3:"facebook"},
+  { hero: "/moodboard/moodboard-4.png", title: "Hello Cyber Bunny" ,media:"/iterenary.jpg,",location:"Head southwest on 5th Ave toward E 41st St",summary:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen.",socialLink1:"facebook",socialLink2:"facebook",socialLink3:"facebook"},
 
  
 ]);
@@ -29,7 +33,7 @@ const MoodBoard = () => {
           /></button>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-4 mx-auto mb-12">
             <div className="flex flex-col space-y-4">
                   <SeconderyButton text={'Hair Transplant'} shadow={'#2DAD9D'}></SeconderyButton>
                   <SeconderyButton text={'Hair Transplant'} shadow={'#2DAD9D'}></SeconderyButton>
